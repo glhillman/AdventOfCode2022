1aae9853a5355cd3058d94d84b90fd2760f6df38
glhillman <glhillman@gmail.com>
2022-12-12 13:01:35 -0700

    Day11

---
+++ Day11/Monkey.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    public class Monkey
+    {
+        public Monkey(int id, string operations, long divisor, int trueTarget, int falseTarget, string startItems)
+        {
+            Id = id;
+            Divisor = divisor;
+            TrueTarget = trueTarget;
+            FalseTarget = falseTarget;
+            OperationDefinition = operations.Split(' ');
+            Op2 = OperationDefinition[2] == "old" ? 0 : long.Parse(OperationDefinition[2]);
+            Items = new LinkedList<long>();
+            string[] parts = startItems.Replace(",", "").Split(' ');
+            foreach (string part in parts)
+            {
+                Items.AddFirst(long.Parse(part));
+            }
+        }
+
+        public int Id { get; private set; }
+        public long Divisor { get; private set; }
+        public int TrueTarget { get; private set; }
+        public int FalseTarget { get; private set; }
+        public long Op2 { get; private set; }
+        public static long Reducer { get; set; }
+        public void AddItem(long value) { Items.AddFirst(value); }
+        public bool EvaluateNextItem(ref long newValue, bool reduceByDivision)
+        {
+            long old = Items.Last.Value;
+            
+            Items.RemoveLast();
+
+            long op1 = old; // always
+            long op2 = Op2 > 0 ? Op2 : op1;
+
+            newValue = OperationDefinition[1] == "+" ? op1 + op2 : op1 * op2;
+
+            newValue = reduceByDivision ? newValue / Reducer : newValue % Reducer;
+
+            return newValue % Divisor == 0;
+        }
+
+        public string[] OperationDefinition;
+        public LinkedList<long> Items;
+    }
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,110 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day11;
+using System.Security.Cryptography.X509Certificates;
+
+DayClass day = new DayClass();
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+day.Part1();
+day.Part2();
+
+watch.Stop();
+Console.WriteLine("Execution Time: {0} ms", watch.ElapsedMilliseconds);
+
+Console.Write("Press Enter to continue...");
+Console.ReadLine();
+
+internal class DayClass
+{
+    public List<Monkey> _monkeys;
+    public DayClass()
+    {
+    }
+
+    public void Part1()
+    {
+        LoadData();
+        Console.WriteLine("Part1: {0}", CalculateMonkeyBusiness(3, true, 20));
+    }
+
+    public void Part2()
+    {
+        LoadData();
+        // create least common multiple for reducer
+        long reducer = 1;
+        foreach (Monkey monkey in _monkeys)
+        {
+            reducer *= monkey.Divisor;
+        }
+        Console.WriteLine("Part2: {0}", CalculateMonkeyBusiness(reducer, false, 10_000));
+    }
+
+    private long CalculateMonkeyBusiness(long reducer, bool reduceByDivision, int nRounds)
+    {
+        Monkey.Reducer = reducer;
+
+        long[] inspectionCount = new long[_monkeys.Count];
+
+        for (int i = 0; i < nRounds; i++)
+        {
+            foreach (Monkey monkey in _monkeys)
+            {
+                while (monkey.Items.Count > 0)
+                {
+                    long newValue = 0;
+
+                    if (monkey.EvaluateNextItem(ref newValue, reduceByDivision) == true)
+                    {
+                        _monkeys[monkey.TrueTarget].AddItem(newValue);
+                    }
+                    else
+                    {
+                        _monkeys[monkey.FalseTarget].AddItem(newValue);
+                    }
+
+                    inspectionCount[monkey.Id]++;
+                }
+            }
+        }
+
+        Array.Sort(inspectionCount);
+
+        int nItems = inspectionCount.Length;
+
+        return inspectionCount[nItems - 1] * inspectionCount[nItems - 2];
+    }
+    private void LoadData()
+    {
+        string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+        if (File.Exists(inputFile))
+        {
+            _monkeys= new List<Monkey>();
+
+            string? line;
+            StreamReader file = new StreamReader(inputFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                int id = line[7] - '0';
+                line = file.ReadLine();
+                string items = line[18..];
+                line = file.ReadLine();
+                string operations = line[19..];
+                line = file.ReadLine();
+                long divisor = long.Parse(line[20..]);
+                line = file.ReadLine();
+                int trueTarget = int.Parse(line.Substring(line.Length - 2));
+                line = file.ReadLine();
+                int falseTarget = int.Parse(line.Substring(line.Length - 2));
+                file.ReadLine();
+
+                _monkeys.Add(new Monkey(id, operations, divisor, trueTarget, falseTarget, items));
+            }
+
+            file.Close();
+        }
+    }
+
+}
