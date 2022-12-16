4c335dab4fddc60e5aa74ebef31d1c28ed0a1e54
glhillman <glhillman@gmail.com>
2022-12-16 05:29:49 -0700

    Day15

---
+++ Day15/Program.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day15;
+
+DayClass day = new DayClass();
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+day.Part1();
+day.Part2();
+
+watch.Stop();
+Console.WriteLine("Execution Time: {0} ms", watch.ElapsedMilliseconds);
+
+Console.Write("Press Enter to continue...");
+Console.ReadLine();
+
+public record Point(long X, long Y);
+
+internal class DayClass
+{
+    List<SBPair> _sbPairs = new();
+
+    public DayClass()
+    {
+        LoadData();
+    }
+
+    public void Part1()
+    {
+
+        long minX = long.MaxValue;
+        long maxX = long.MinValue;
+        long targetRow = 2_000_000;
+
+        foreach (SBPair pair in _sbPairs)
+        {
+            long minx;
+            long maxx;
+            bool isValid;
+
+            (minx, maxx, isValid) = pair.MinMaxForY(targetRow);
+            if (isValid)
+            {
+                minX = Math.Min(minX, minx);
+                maxX = Math.Max(maxX, maxx);
+            }
+        }
+
+        Console.WriteLine("Part1: {0}", maxX - minX);
+    }
+
+    public void Part2()
+    {
+        long rslt = 0;
+        long minx;
+        long maxx;
+
+        List<(long min, long max)> rowPairs = new();
+        bool found = false;
+
+        // count down, assuming that the fault will be closer to the bottom than the top (just sayin')
+        for (int row = 4_000_000; !found && row >= 0; row--)
+        {
+            rowPairs.Clear();
+            foreach (SBPair pair in _sbPairs)
+            {
+                bool isValid;
+
+                (minx, maxx, isValid) = pair.MinMaxForY(row);
+                if (isValid)
+                {
+                    rowPairs.Add((minx, maxx));
+                }
+            }
+
+            // sort the pairs by the minimum x
+            // the gap will appear on a line where the min of a min/max pair is greater than the max for the line so far
+            rowPairs.Sort((rec1, rec2) => (int)(rec1.min - rec2.min));
+            minx = rowPairs[0].min;
+            maxx = rowPairs[0].max;
+            foreach ((long min, long max) rec in rowPairs)
+            {
+                if (rec.min > maxx)
+                {
+                    // we've found the gap! 
+                    rslt = (maxx + 1) * 4_000_000 + row;
+                    found = true;
+                    break;
+                } else if (rec.max >= maxx)
+                {
+                    maxx = rec.max;
+                }
+            }
+        }
+
+        Console.WriteLine("Part2: {0}", rslt);
+    }
+
+    private void LoadData()
+    {
+        string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+        if (File.Exists(inputFile))
+        {
+            string? line;
+            StreamReader file = new StreamReader(inputFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] nums = line.Replace("Sensor at x=", "").Replace(" y=", "").Replace(": closest beacon is at x=", ",").Replace(" ", "").Split(',');
+                _sbPairs.Add(new SBPair(new Point(long.Parse(nums[0]), long.Parse(nums[1])), new Point(long.Parse(nums[2]), long.Parse(nums[3]))));
+            }
+
+            file.Close();
+        }
+    }
+
+}
---
+++ Day15/SBPair.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    // Sensor/Beacon pair
+    public class SBPair
+    {
+        public SBPair(Point sensor, Point beacon)
+        {
+            Sensor = sensor;
+            Beacon = beacon;
+            Distance = Math.Abs(sensor.X - beacon.X) + Math.Abs(sensor.Y - beacon.Y);
+        }
+
+        public Point Sensor { get; private set; }
+        public Point Beacon { get; private set; }
+        public long Distance { get; private set; }
+        public (long minX, long maxX, bool isValid) MinMaxForY(long y)
+        {
+            long minx = 0;
+            long maxx = 0;
+            bool isValid = false;
+
+            if (y >= Sensor.Y && y <= Sensor.Y + Distance)
+            {
+                // y is below Sensor, but within sensor coverage
+                long temp = Distance - (y - Sensor.Y);
+                minx = Sensor.X - temp;
+                maxx = Sensor.X + temp;
+                isValid = true;
+            }
+            else if (y <= Sensor.Y && y >= Sensor.Y - Distance)
+            {
+                // y is above Sensor, but within its coverage
+                long temp = Distance - (Sensor.Y - y);
+                minx = Sensor.X - temp;
+                maxx = Sensor.X + temp;
+                isValid = true;
+            }
+            return (minx, maxx, isValid);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Sensor: {0}, Beacon: {1}, Distance: {2}", Sensor, Beacon, Distance);
+        }
+    }
+}
