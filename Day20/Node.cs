f51bfbaa1fc9b2c4fcdb4989dc512f59d8913147
glhillman <glhillman@gmail.com>
2022-12-23 07:36:09 -0700

    Day20. Finally. Ug.

---
+++ Day20/Node.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Day20
+{
+    internal class Node
+    {
+        public Node(long value)
+        {
+            Value = value;
+        }
+
+        public long Value { get; private set; }
+        public Node Prev { get; set; }
+        public Node Next { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Value: {0}, Prev: {1}, Next: {2}", Value, Prev.Value, Next.Value);
+        }
+    }
+
+    internal class LinkedNodes
+    {
+        public LinkedNodes()
+        {
+            Nodes = new List<Node>();
+        }
+
+        public Node Add(long value)
+        {
+            Node node = new Node(value);
+            if (Nodes.Count == 0)
+            {
+                node.Next = node;
+                node.Prev = node;
+            }
+            else
+            {
+                node.Prev = Nodes[Nodes.Count - 1];
+                node.Next = Nodes[0];
+                Nodes[0].Prev = node;
+                Nodes[Nodes.Count - 1].Next = node;
+            }
+            Nodes.Add(node);
+            NodeCount++;
+            ModValue = NodeCount - 1;
+
+            return node;
+        }
+
+        public void MixNode(int nodeIndex)
+        {
+            // optimized mix logic from Jeff & Scott
+            long steps = Nodes[nodeIndex].Value;
+            long delta = Math.Abs(steps % ModValue);
+
+            if (delta > NodeCount / 2)
+            {
+                delta = NodeCount - delta - 1;
+                steps = -steps;
+            }
+
+            for (int i = 0; i < delta; i++)
+            {
+                if (steps < 0)
+                {
+                    Swap(Nodes[nodeIndex].Prev, Nodes[nodeIndex]);
+                }
+                else
+                {
+                    Swap(Nodes[nodeIndex], Nodes[nodeIndex].Next);
+                }
+            }
+
+        }
+
+        public long GetGroveCoordinates()
+        {
+            Node node = Nodes.Find(n => n.Value == 0);
+            long sum = 0;
+            for (int i = 0; i <= 3000; i++)
+            {
+                if (i % 1000 == 0)
+                {
+                    sum += node.Value;
+                }
+                node = node.Next;
+            }
+
+            return sum;
+        }
+
+        private void Swap(Node left, Node right)
+        {
+            Node leftPrev = left.Prev;
+            Node next = right.Next;
+
+            next.Prev = left;
+            left.Prev = right;
+            left.Next = next;
+            right.Prev = leftPrev;
+            right.Next = left;
+            leftPrev.Next = right;
+        }
+
+        public void DumpNodes(string msg)
+        {
+            Console.WriteLine(msg);
+            Node rootNode = Nodes[0];
+            Node node = rootNode;
+            do
+            {
+                Console.Write(node.Value + " ");
+                node = node.Next;
+            }
+            while (node != rootNode);
+            Console.WriteLine();
+        }
+
+        public List<Node> Nodes { get; set; }
+        public long ModValue {get; set;}
+        public long NodeCount { get; set;}
+
+    }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day20;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+DayClass day = new DayClass();
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+day.Part1();
+day.Part2();
+
+watch.Stop();
+Console.WriteLine("Execution Time: {0} ms", watch.ElapsedMilliseconds);
+
+Console.Write("Press Enter to continue...");
+Console.ReadLine();
+
+internal class DayClass
+{
+    LinkedNodes _nodes;
+
+    public DayClass()
+    {
+    }
+
+    public void Part1()
+    {
+        LoadData(1);
+
+        for (int i = 0; i < _nodes.NodeCount; i++)
+        {
+            _nodes.MixNode(i);
+        }
+
+        long groveCoords = _nodes.GetGroveCoordinates();
+
+        Console.WriteLine("Part1: {0}", groveCoords);
+    }
+
+    public void Part2()
+    {
+        LoadData(811589153);
+
+        for (int mix = 0; mix < 10; mix++)
+        {
+            for (int i = 0; i < _nodes.NodeCount; i++)
+            {
+                _nodes.MixNode(i);
+            }
+
+        }
+        long groveCoords = _nodes.GetGroveCoordinates();
+
+        Console.WriteLine("Part2: {0}", groveCoords);
+    }
+
+
+    private void LoadData(long decryptKey)
+    {
+        string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+        if (File.Exists(inputFile))
+        {
+            string[] strings= File.ReadAllLines(inputFile);
+            _nodes = new LinkedNodes();
+            for (int i = 0; i < strings.Length; i++)
+            {
+                _nodes.Add(long.Parse(strings[i]) * decryptKey);
+            }
+        }
+    }
+}
